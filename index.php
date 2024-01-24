b6f9b77b99b3a85ff978a253bd190f5379455038
Aleksey Anufrienok <alex.anufrienok@gmail.com>
2024-01-24 19:53:23 +0300

    install opencart 4.0.2.3

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
-phpinfo();
+// Version
+define('VERSION', '4.0.2.3');
+
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
+
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit();
+}
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+
+// Framework
+require_once(DIR_SYSTEM . 'framework.php');
